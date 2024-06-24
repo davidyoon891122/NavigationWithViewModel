819014e1d774d1980f1059418df286d6d28c15ea
David Yoon <davidyoon@hanwha.com>
2024-06-24 13:45:38 +0900

    Initial Commit

---
+++ NavigatorAndViewModel/NavigatorAndViewModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigatorAndViewModelApp.swift
+//  NavigatorAndViewModel
+//
+//  Created by Davidyoon on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigatorAndViewModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
